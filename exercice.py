57fe5b11c8bfec31f427f98dec0d37ad2a1df87f
ndiayendeye <ndiaye.ndeye1@uam.edu.sn>
2022-04-01 00:36:11 +0200

    le fichier est mis a jour

---
+++ exercice.py
@@ -0,0 +1,8 @@
+class Personne:
+	def __init__(self , n,p):
+		self.n=nom
+		self.p=prenom
+n=input("entrez votre nom")
+p=input("entrez votre prenom")
+P=Persone(n,p)
+print("la personne s'appelle",P.nom,P.prenom)		
